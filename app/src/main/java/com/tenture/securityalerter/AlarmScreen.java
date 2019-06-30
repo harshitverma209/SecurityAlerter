666db9d1a4c5e5d8772fa84a9293e1196d85b1da
Awesome <harshitverma209@gmail.com>
2019-06-30 22:09:40 +0530

    / ‘app/src/main/java/com/tenture/securityalerter/AlarmScreen.java’

--- app/src/main/java/com/tenture/securityalerter/AlarmScreen.java
+++ app/src/main/java/com/tenture/securityalerter/AlarmScreen.java
@@ -60,7 +60,7 @@ public class AlarmScreen extends Activity {
             @Override
             public void onClick(View view) {
                 HashMap<String, String> params = new HashMap<String,String>();
-                params.put("userid", getIntent().getStringExtra("userid"));
+                params.put("userid", userid);
                 params.put("lat", getIntent().getStringExtra("lat")); // the entered data as the body.
                 params.put("lon", getIntent().getStringExtra("lon"));
                 JSONObject jsonObject= new JSONObject(params);
